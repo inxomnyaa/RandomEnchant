c4880a802e6b056fa3e6bea85d639258088ad456
thebigsmileXD <thebigsmilexd@gmail.com>
2017-01-26 20:13:36 +0100

    Fix #1

--- src/xenialdan/RandomEnchant/EventListener.php
+++ src/xenialdan/RandomEnchant/EventListener.php
@@ -7,6 +7,7 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\item\Armor;
 use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\Item;
 use pocketmine\item\Tool;
 use pocketmine\plugin\Plugin;
 
@@ -25,8 +26,11 @@ class EventListener implements Listener {
 	 * @return bool
 	 */
 	public function onClickTable(PlayerInteractEvent $event) {
-		if (($player = $event->getPlayer())->hasPermission('randomenchant') && $player->getInventory()->getItemInHand() instanceof Tool || $player->getInventory()->getItemInHand() instanceof Armor && $event->getBlock()->getId() === Block::ENCHANT_TABLE) {
-			$player->getInventory()->getItemInHand()->addEnchantment(Enchantment::getEnchantment(array_rand(Enchantment::$enchantments)));
+		if (($player = $event->getPlayer())->hasPermission('randomenchant') && ($item = $player->getInventory()->getItemInHand()) instanceof Tool || $player->getInventory()->getItemInHand() instanceof Armor && $event->getBlock()->getId() === Block::ENCHANT_TABLE) {
+			while (!$item->hasEnchantments()) {
+				$player->getInventory()->getItemInHand()->addEnchantment(Enchantment::getEnchantment(mt_rand(0, 24)));
+			}
+			$event->setCancelled();
 		}
 		return true;
 	}
