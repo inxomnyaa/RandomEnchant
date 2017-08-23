1939b79af8401915c411b3c606a947d1d0e1b129
thebigsmileXD <thebigsmilexd@gmail.com>
2017-08-23 02:32:32 +0200

    1.2beta and below update

--- src/xenialdan/RandomEnchant/EventListener.php
+++ src/xenialdan/RandomEnchant/EventListener.php
@@ -7,17 +7,17 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\item\Armor;
 use pocketmine\item\enchantment\Enchantment;
-use pocketmine\item\Item;
 use pocketmine\item\Tool;
 use pocketmine\plugin\Plugin;
 
-class EventListener implements Listener {
+class EventListener implements Listener{
+	private $owner;
 
 	/**
 	 * EventListener constructor.
 	 * @param Plugin $plugin
 	 */
-	public function __construct(Plugin $plugin) {
+	public function __construct(Plugin $plugin){
 		$this->owner = $plugin;
 	}
 
@@ -25,17 +25,15 @@ class EventListener implements Listener {
 	 * @param PlayerInteractEvent $event
 	 * @return bool
 	 */
-	public function onClickTable(PlayerInteractEvent $event) {
-		if (($player = $event->getPlayer())->hasPermission('randomenchant') && $event->getBlock()->getId() === Block::ENCHANT_TABLE) {
-			$item = $player->getInventory()->getItemInHand();
-			if ($item instanceof Tool || $item instanceof Armor) {
-				while (!$item->hasEnchantments()) {
-					$enchantment = Enchantment::getEnchantment(mt_rand(0, 24));
-					if($enchantment->getId() === Enchantment::TYPE_INVALID)
-						continue;
+	public function onClickTable(PlayerInteractEvent $event){
+		if (($player = $event->getPlayer())->hasPermission('randomenchant') && $event->getBlock()->getId() === Block::ENCHANTING_TABLE){
+			$item = $event->getItem();
+			if ($item instanceof Tool || $item instanceof Armor){
+				do{
+					$enchantment = Enchantment::getEnchantment(mt_rand(Enchantment::PROTECTION, Enchantment::MENDING));
+					if ($item->hasEnchantment($enchantment->getId())) continue;
 					$item->addEnchantment($enchantment);
-				}
-				$player->getInventory()->setItemInHand($item);
+				} while (!$item->hasEnchantment($enchantment->getId()));
 				$event->setCancelled();
 			}
 		}
--- src/xenialdan/RandomEnchant/Loader.php
+++ src/xenialdan/RandomEnchant/Loader.php
@@ -2,17 +2,10 @@
 
 namespace xenialdan\RandomEnchant;
 
-use pocketmine\block\Block;
-use pocketmine\item\Item;
 use pocketmine\plugin\PluginBase;
-use pocketmine\tile\Tile;
-use xenialdan\SimpleSpawner\block\MonsterSpawner;
-use xenialdan\SimpleSpawner\tile\MobSpawner;
 
-class Loader extends PluginBase
-{
-    public function onEnable()
-    {
+class Loader extends PluginBase{
+	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
-    }
+	}
 }
